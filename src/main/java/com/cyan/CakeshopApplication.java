c0dfc5f1b3f52bc4f3ed24fa7d7f4223f6b22d25
Lin9207 <56595118+Lin9207@users.noreply.github.com>
2020-07-29 11:02:50 +0800

    更新

--- src/main/java/com/cyan/CakeshopApplication.java
+++ src/main/java/com/cyan/CakeshopApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
 @ServletComponentScan // 自动扫描带有(@WebServlet,@WebFilter,@WebListener)注解的类，完成注册。
 public class CakeshopApplication {
-
     public static void main(String[] args) {
-
         SpringApplication.run(CakeshopApplication.class, args);
     }
 }
